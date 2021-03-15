063fad3623ee52895078fb32ce556e963c4c44d7
CupOfEarlGrey <gomelsky99@gmail.com>
2021-03-15 20:26:56 +0200

    Lab_1 commit

--- library_main/models.py
+++ library_main/models.py
@@ -1,3 +1,21 @@
+from os import path
+from uuid import uuid4
 from django.db import models
 
+
 # Create your models here.
+def get_file_name(self, filename):
+    ext = filename.strip().split('.')[-1]
+    filename = f"{uuid4()}.{ext}"
+    return path.join("images", filename)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=get_file_name)
+    is_visible = models.BooleanField(default=True)
+    book_order = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.author}"
